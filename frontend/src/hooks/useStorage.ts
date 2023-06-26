4098bd75bda1b7c5195fe4c3334434857b633cc9
Arthur Silva <arthurmatsilva@outlook.com>
2023-06-26 15:55:05 -0300

    fix: json parse problem

--- frontend/src/hooks/useStorage.ts
+++ frontend/src/hooks/useStorage.ts
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const useStorage = <T,>(key: string, initialValue?: T) => {
-  const [value, setValue] = React.useState<T>(
-    JSON.parse(localStorage.getItem(key) || "") || initialValue
-  );
+export const useStorage = <T,>(key: string, initialValue: T) => {
+  const [value, setValue] = React.useState<T>(() => {
+    return JSON.parse(localStorage.getItem(key) || "") || initialValue;
+  });
 
   React.useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
